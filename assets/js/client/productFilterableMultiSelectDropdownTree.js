876c8b5bd1d19badcad79d5fdc5fcb9a5b7e6559
Bart Daniels <bart.daniels@vito.be>
2018-06-13 13:06:16 +0200

    [2] only allow product tree selections on the same level

--- assets/js/client/productFilterableMultiSelectDropdownTree.js
+++ assets/js/client/productFilterableMultiSelectDropdownTree.js
@@ -12,7 +12,27 @@ class ProductFilterableMultiSelectDropdownTree extends FilterableMultiSelectDrop
     }
 
     handleOnChange(value) {
-        this.state.callback(value);
+        var new_value = [];
+        if (value !== undefined) {
+            new_value = this.selectValue(4, value);
+        }
+        this.state.callback(new_value);
+    }
+
+    selectValue(level, value) {
+        var new_value = this.state.data.filter(d => d.level == level && value.includes(d.value)).map(d => d.value);
+        if (level > 1) {
+            if (new_value.length == 0) {
+                new_value = this.selectValue(level - 1, value);
+            } else if (new_value.length == 1 && value.length > 2) {
+            //    check if there are higher levels with more selections
+                var new_value_2 = this.selectValue(level - 1, value);
+                if (new_value_2.length > 1) {
+                    new_value = new_value_2;
+                }
+            }
+        }
+        return new_value;
     }
 
     componentWillMount() {
@@ -44,7 +64,7 @@ class ProductFilterableMultiSelectDropdownTree extends FilterableMultiSelectDrop
         // result = {data, errors, meta}
         var data = [];
         for (var product of result.data) {
-            data.push({id: product.Global_id, pId: product.Parent_Id, value: product.Global_id.toString(), label: product.Name});
+            data.push({id: product.Global_id, pId: product.Parent_Id, value: product.Global_id.toString(), label: product.Name, level: product.Level});
         }
         this.setState({disabled: this.state.disabled, data: data, value: this.state.value, placeholder: "select product(s)", callback: this.state.callback});
     }
