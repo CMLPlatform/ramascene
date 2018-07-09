c2461ec8bb3614b5377bbc12759b2824918ae473
SidneyNiccolson <sidneyniccolson@gmail.com>
2018-07-09 14:21:49 +0200

    v0.02: Celery routes for different jobs

--- ramasceneMasterProject/celery.py
+++ ramasceneMasterProject/celery.py
@@ -6,3 +6,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ramasceneMasterProject.settings
 app = Celery('ramasceneMasterProject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.task_routes = ([
+    ('calc_default.tasks', {'queue': 'calc'}),
+    ('calc_timeseries.tasks', {'queue': 'timeseries'})
+],)
