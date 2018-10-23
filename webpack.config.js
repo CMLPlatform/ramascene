0d3930c07a1a960ead0a4162d007484e7c73bc33
dierckxk <koen.dierckx@vito.be>
2018-10-23 12:14:47 +0200

    environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,70 @@
 //require our dependencies
 var path = require('path');
 var webpack = require('webpack');
-const { resolve } = require('path');
+const {resolve} = require('path');
 var BundleTracker = require('webpack-bundle-tracker');
+const Dotenv = require('dotenv-webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const APP_DIR = resolve(__dirname, 'assets/js/client');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const APP_DIR = resolve(__dirname, 'assets/js/client');
 
 module.exports = env => {
-  return {
-  //the base directory (abs. path) for resolving the entry option
-  context: __dirname,
-  entry: ['babel-polyfill', APP_DIR + '/ramascene.js'],
+    return {
+        //the base directory (abs. path) for resolving the entry option
+        context: __dirname,
+        entry: ['babel-polyfill', APP_DIR + '/ramascene.js'],
 
-  output: {
-    //where to store compiled bundle
-    path: path.resolve(__dirname + '/assets/bundles/'),
+        output: {
+            //where to store compiled bundle
+            path: path.resolve(__dirname + '/assets/bundles/'),
 
-    //webpack naming convention where files are stores
-    filename: '[name]-[hash].js'
-  },
+            //webpack naming convention where files are stores
+            filename: '[name]-[hash].js'
+        },
 
-  plugins: [
-    //where to store meta-data about the bundle
-    new BundleTracker({ path: __dirname, filename:  './webpack-stats.json' }),
-    new ExtractTextPlugin({
-        filename:'style.css',
-        disable: false,
-        allChunks: true
+        plugins: [
+            //where to store meta-data about the bundle
+            new BundleTracker({path: __dirname, filename: './webpack-stats.json'}),
+            new Dotenv(),
+            new ExtractTextPlugin({
+                filename: 'style.css',
+                disable: false,
+                allChunks: true
 
-    }),
-    new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('dev')
-    }
-  }),
-  /* PRODUCTION SETTINGS -> change above to production value if needed
-   new UglifyJsPlugin(),
-   */
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('dev')
+                }
+            }),
+            /* PRODUCTION SETTINGS -> change above to production value if needed
+             new UglifyJsPlugin(),
+             */
 
-    
-    new webpack.DefinePlugin({
-                'WEBSOCKET_URL': '"ws://' + env.HOSTNAME + '/ramascene/"',
-                'AJAX_URL': '"http://' + env.HOSTNAME + '/ajaxhandling/"'
+
+            new webpack.DefinePlugin({
+                'WEBSOCKET_URL': '"ws://' + process.env.HOSTNAME + '/ramascene/"',
+                'AJAX_URL': '"http://' + process.env.HOSTNAME + '/ajaxhandling/"'
                 // 'WEBSOCKET_URL': '"ws://cml.liacs.nl:8080/ramascene/"',
                 // 'AJAX_URL': '"http://cml.liacs.nl:8080/ajaxhandling/"'
             })
-  ],
+        ],
 
-  module: {
+        module: {
             loaders: [
                 {
                     include: APP_DIR,
                     loader: 'babel-loader',
                     query: {
-                      //what will be dealing with (react code)
-                      presets: ['es2015', 'react'],
-                      plugins: ['transform-class-properties']
+                        //what will be dealing with (react code)
+                        presets: ['es2015', 'react'],
+                        plugins: ['transform-class-properties']
                     },
                     test: /\.js$/
                 },
                 {
                     test: /\.(css|sass|scss)$/,
-                    use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' })
+                    use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader!sass-loader'})
                 },
                 {
                     test: /\.woff2?$|\.ttf$|\.svg$|\.eot$/,
@@ -75,9 +77,10 @@ module.exports = env => {
             ]
         },
 
-  resolve: {
-    //where to look for modules
+        resolve: {
+            //where to look for modules
 
-    extensions: ['.js', '.jsx', '.css']
-  }
-}};
+            extensions: ['.js', '.jsx', '.css']
+        }
+    }
+};
