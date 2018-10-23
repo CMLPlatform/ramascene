885febc527d8a6ff27ece8d4b902972a915e795e
dierckxk <koen.dierckx@vito.be>
2018-10-23 13:57:25 +0200

    temporary test

--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,10 @@ module.exports = env => {
 
 
             new webpack.DefinePlugin({
-                'WEBSOCKET_URL': '"ws://' + process.env.HOST + '/ramascene/"',
-                'AJAX_URL': '"http://' + process.env.HOST + '/ajaxhandling/"'
+                // 'WEBSOCKET_URL': '"ws://' + process.env.HOST + '/ramascene/"',
+                // 'AJAX_URL': '"http://' + process.env.HOST + '/ajaxhandling/"'
+                'WEBSOCKET_URL': '"ws://ramascene.local/ramascene/"',
+                'AJAX_URL': '"http://ramascene.local/ajaxhandling/"'
                 // 'WEBSOCKET_URL': '"ws://cml.liacs.nl:8080/ramascene/"',
                 // 'AJAX_URL': '"http://cml.liacs.nl:8080/ajaxhandling/"'
             })
