3e8388e9146cbff11aea7929f4bf21de7267485f
dierckxk <koen.dierckx@vito.be>
2018-10-23 12:00:21 +0200

    replaced env.RELEASE_PATH with __dirname

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = env => {
 
   output: {
     //where to store compiled bundle
-    path: path.resolve(env.RELEASE_PATH + '/assets/bundles/'),
+    path: path.resolve(__dirname + '/assets/bundles/'),
 
     //webpack naming convention where files are stores
     filename: '[name]-[hash].js'
@@ -23,7 +23,7 @@ module.exports = env => {
 
   plugins: [
     //where to store meta-data about the bundle
-    new BundleTracker({ path: env.RELEASE_PATH, filename:  './webpack-stats.json' }),
+    new BundleTracker({ path: __dirname, filename:  './webpack-stats.json' }),
     new ExtractTextPlugin({
         filename:'style.css',
         disable: false,
