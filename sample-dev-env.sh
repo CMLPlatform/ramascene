d8b66bf07e6bed89d409520116adc33dd94a0219
SidneyNiccolson <sidneyniccolson@gmail.com>
2018-11-23 14:52:24 +0100

    sample for setting env. variables

---
+++ sample-dev-env.sh
@@ -0,0 +1,4 @@
+export DJANGO_SETTINGS_MODULE=ramasceneMasterProject.config.dev
+export DATASETS_VERSION=v3
+export DATASETS_DIR=[my/path/to/datasets]
+
---
+++ sample-prod-env.sh
@@ -0,0 +1,4 @@
+export DJANGO_SETTINGS_MODULE=ramasceneMasterProject.config.production
+export DATASETS_VERSION=v3
+export DATASETS_DIR=<my/path/to/datasets>
+export SECRET_KEY=<your secret key>
