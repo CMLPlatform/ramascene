b47e58e693f8ea2df270617d2698b5238acab9b0
SidneyNiccolson <sidneyniccolson@gmail.com>
2018-11-29 17:28:54 +0100

    testing RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('../..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ramasceneMasterProject.config.dev'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ramasceneMasterProject.config.base'
 django.setup()
 
 
