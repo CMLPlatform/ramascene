43e3ee199884b5682a675def5566d8327863a0ad
dierckxk <koen.dierckx@vito.be>
2019-03-12 11:09:02 +0100

    celery is using 2 named services

--- config/deploy/staging_celery.rb
+++ config/deploy/staging_celery.rb
@@ -1,2 +1,2 @@
 set :application, 'ramascene_staging_celery'
-server 'web02.ramascene.sas.vito.local', user: 'jenkins', roles: [:job], systemd_units: [fetch(:sas_systemd_celery), fetch(:sas_systemd_flower)]
+server 'web02.ramascene.sas.vito.local', user: 'jenkins', roles: [:job], systemd_units: [fetch(:sas_systemd_celery_calculation), fetch(:sas_systemd_celery_modelling), fetch(:sas_systemd_flower)]
