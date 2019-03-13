6c5c645974a848cc8421ee5a936be4af3cd9dbbb
dierckxk <koen.dierckx@vito.be>
2019-03-13 13:53:53 +0100

    allow sqlite db to be configured using settings

--- ramasceneMasterProject/config/base.py
+++ ramasceneMasterProject/config/base.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'ramasceneMasterProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get('DATABASES_DEFAULT_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
