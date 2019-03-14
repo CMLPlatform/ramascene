f100545ba8b1c6949672203884266e2876f529cf
dierckxk <koen.dierckx@vito.be>
2019-03-14 11:58:12 +0100

    save space in development

--- config/deploy/development_celery_modelling.rb
+++ config/deploy/development_celery_modelling.rb
@@ -1,2 +1,3 @@
 set :application, 'ramascene_staging_celery_modelling'
 server 'p4dev.sas.vito.local', user: 'jenkins', roles: [:job], systemd_units: [fetch(:sas_systemd_celery)]
+set :keep_releases, 1
